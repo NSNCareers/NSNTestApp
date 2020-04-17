f68e83a62e61a50aabbf8167cc8d702e87a2cb5a
Ajang Ngwesse <elvisdemoprojects@outlook.com>
2020-04-17 08:56:45 +0100

    changes

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace LoginApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContextPool<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DockerConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
