50fa15a5ec37344e752e487dcd98a8bf248ecedb
Ajang elvis Ngwesse <NSNCareers@outlook.com>
2020-06-15 00:58:25 +0100

    app changes

--- Startup.cs
+++ Startup.cs
@@ -92,6 +92,7 @@ namespace LoginApp
             else
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
